204a5f35b5f0df573796c20c6a9e6f89f96b12fa
istiyak1264 <156337741+istiyak1264@users.noreply.github.com>
2024-02-01 13:11:43 +0600

    112A _ petya and string.c
    website:  codeforces.com
    problem:  112A. Petya and Strings 
    
    time limit per test2 seconds
    memory limit per test256 megabytes

---
+++ 112A  petya and string.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <string.h>
+int main ()
+{
+    char s1[100];
+    char s2[100];
+    gets(s1);
+    gets(s2);
+
+    int value = stricmp(s1,s2);
+
+    if(value > 0)
+    {
+        printf("1\n");
+    }
+
+    else if(value < 0)
+    {
+        printf("-1\n");
+    }
+
+    else
+    {
+        printf("0\n");
+    }
+
+    return(0);
+ }
